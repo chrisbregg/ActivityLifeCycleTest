d338365b6396c19d515982e9c175a4d706d085e9
Chris Bregg <chrisbregg@gmail.com>
2024-03-15 11:20:55 -0700

    Implemented lifecycle functions

--- app/src/main/java/com/example/activitylifecycletest/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycletest/MainActivity.kt
@@ -27,6 +27,30 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 }
 
 @Composable
